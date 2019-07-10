b88e86d9d38b0a02665cc817fa80468dd4ade7b6
nastromo <nastromo@Solaris.local>
2019-07-10 18:45:53 +0300

    deploy update

--- src/utils/Api.js
+++ src/utils/Api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-let url = `http://192.168.20.211:9000`;
+let url = `http://192.168.20.218:9000`;
 if (window.location.href.includes(`localhost`)) {
     url = `http://localhost:9000`;
 }
